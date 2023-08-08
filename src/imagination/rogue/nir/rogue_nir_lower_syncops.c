6cf7ea845753d7c76f2bbb7b2718be7f48e9e78c
Simon Perretta <simon.perretta@imgtec.com>
2023-08-08 18:01:54 +0100

    pvr: Improve per-instance loop and atomic op emulation
    Emit loop phi nodes properly to satisfy lcssa
    
    Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>

--- src/imagination/rogue/nir/rogue_nir_lower_syncops.c
+++ src/imagination/rogue/nir/rogue_nir_lower_syncops.c
@@ -41,95 +41,227 @@ struct options {
    nir_variable_mode modes;
 };
 
-/*
-for (unsigned u = 0; u < instances_in_slot; ++u)
-   if (u == instance_num)
-      <per_instance_body>
-*/
-static nir_def *rogue_per_instance_loop(nir_builder *b,
-                                        bool locked,
-                                        unsigned result_num_components,
-                                        unsigned result_bit_size)
+/**
+ * \brief Emits a (optionally mutex-locked) loop that will execute an operation
+ * once per each active instance.
+ *
+ * \param[in] b The NIR builder context.
+ * \param[in] locked Whether to mutex-lock the loop (across slots).
+ * \param[in] result_num_components The number of components for the result.
+ * \param[in] result_bit_size The bit size for the result.
+ * \param[in] per_inst_op A pointer to the dummy per-instance op to be replaced.
+ * \param[in] loop_result A pointer to the phi node containing the loop result.
+ *
+ * %inst_num = @load_instance_num_img()
+ * %iter_start = 0
+ * %iter_check = ROGUE_MAX_INSTANCES_PER_TASK
+ * %iter_incr = 1
+ * %undef = undefined
+ *
+ * loop {
+ *    %phi_iter_ssa = phi %iter_start, %next_iter
+ *    %phi_result_out_ssa = phi %undef, %phi_result_ssa
+ *
+ *    if (%phi_iter_ssa >= %iter_check)
+ *       break;
+ *
+ *    if (%phi_iter_ssa == %inst_num)
+ *       %per_inst_op = ...
+ *
+ *    %phi_result_ssa = phi %per_inst_op, %phi_result_out_ssa
+ *    %next_iter = iadd %phi_iter_ssa, %iter_incr
+ * }
+ */
+static void rogue_per_instance_loop(nir_builder *b,
+                                    bool locked,
+                                    unsigned result_num_components,
+                                    unsigned result_bit_size,
+                                    nir_def **per_inst_op,
+                                    nir_def **loop_result)
 {
    nir_shader *shader = b->shader;
 
-   /* for (unsigned loop_iter = loop_init_val;
-    *      loop_iter < loop_cond_val;
-    *      loop_iter += loop_incr_val)
-    */
+   /* Set up the loop. */
+   nir_def *inst_num = nir_load_instance_num_img(b);
+   nir_def *iter_start = nir_imm_int(b, 0);
+   nir_def *iter_check = nir_imm_int(b, ROGUE_MAX_INSTANCES_PER_TASK);
+   nir_def *iter_incr = nir_imm_int(b, 1);
+   nir_def *undef = nir_undef(b, result_num_components, result_bit_size);
 
-   /* Setup the loop. */
-   nir_def *loop_init_val = nir_imm_int(b, 0);
-   nir_def *loop_cond_val = nir_imm_int(b, ROGUE_MAX_INSTANCES_PER_TASK);
-   nir_def *loop_incr_val = nir_imm_int(b, 1);
-   nir_def *op_result = NULL;
-
-   if (locked)
+   if (locked) {
       nir_mutex_img(b,
                     .mutex_id_img = ROGUE_MUTEX_ID_ATOMIC_EMU,
                     .mutex_op_img = ROGUE_MUTEX_OP_LOCK);
+   }
 
    /* Create phi for loop iterator. */
    nir_phi_instr *phi_iter = nir_phi_instr_create(shader);
    nir_def_init(&phi_iter->instr,
                 &phi_iter->def,
-                loop_init_val->num_components,
-                loop_init_val->bit_size);
-   nir_def *loop_iter = &phi_iter->def;
+                iter_start->num_components,
+                iter_start->bit_size);
+   nir_def *phi_iter_ssa = &phi_iter->def;
 
-   /**/
+   /* Create phis for loop result. */
+   nir_phi_instr *phi_result_out = nir_phi_instr_create(shader);
+   nir_def_init(&phi_result_out->instr,
+                &phi_result_out->def,
+                result_num_components,
+                result_bit_size);
+   nir_def *phi_result_out_ssa = &phi_result_out->def;
+
+   nir_phi_instr *phi_result = nir_phi_instr_create(shader);
+   nir_def_init(&phi_result->instr,
+                &phi_result->def,
+                result_num_components,
+                result_bit_size);
+   nir_def *phi_result_ssa = &phi_result->def;
+
+   /* Dummy per-instance op to be replaced. */
+   nir_def *dummy_op;
 
    /* Create loop, don't allow it to be unrolled. */
    nir_loop *per_inst_loop = nir_push_loop(b);
-   per_inst_loop->control = nir_loop_control_dont_unroll;
+   nir_block *pre_loop =
+      nir_block_cf_tree_prev(nir_loop_first_block(per_inst_loop));
    {
-      nir_phi_instr_add_src(phi_iter,
-                            loop_init_val->parent_instr->block,
-                            loop_init_val);
+      nir_phi_instr_add_src(phi_iter, pre_loop, iter_start);
+
+      nir_phi_instr_add_src(phi_result_out, pre_loop, undef);
 
       /* Loop test. */
-      nir_def *loop_cond = nir_uge(b, loop_iter, loop_cond_val);
+      nir_def *loop_cond = nir_uge(b, phi_iter_ssa, iter_check);
       nir_if *loop_break_if = nir_push_if(b, loop_cond);
-      loop_break_if->control = nir_selection_control_dont_flatten;
       {
          nir_jump_instr *loop_break =
             nir_jump_instr_create(shader, nir_jump_break);
          nir_builder_instr_insert(b, &loop_break->instr);
       }
+      loop_break_if->control = nir_selection_control_dont_flatten;
       nir_pop_if(b, loop_break_if);
 
       /* Per-instance loop body. */
-      nir_def *inst_num = nir_load_instance_num_img(b);
-      nir_def *per_inst_cond = nir_ieq(b, loop_iter, inst_num);
+      nir_def *per_inst_cond = nir_ieq(b, phi_iter_ssa, inst_num);
       nir_if *inst_num_if = nir_push_if(b, per_inst_cond);
-      inst_num_if->control = nir_selection_control_dont_flatten;
       {
          /* To be replaced by the caller of this function. */
          nir_undef_instr *undef = nir_undef_instr_create(shader,
                                                          result_num_components,
                                                          result_bit_size);
          nir_builder_instr_insert(b, &undef->instr);
-         op_result = &undef->def;
+         dummy_op = &undef->def;
+
+         nir_phi_instr_add_src(phi_result,
+                               nir_if_last_then_block(inst_num_if),
+                               dummy_op);
       }
+      inst_num_if->control = nir_selection_control_dont_flatten;
       nir_pop_if(b, inst_num_if);
 
-      /* Loop increment src for loop_iter. */
-      nir_def *loop_incr = nir_iadd(b, loop_iter, loop_incr_val);
+      nir_phi_instr_add_src(phi_result_out,
+                            nir_cursor_current_block(b->cursor),
+                            phi_result_ssa);
+
+      nir_phi_instr_add_src(phi_result,
+                            nir_if_last_else_block(inst_num_if),
+                            phi_result_out_ssa);
+      nir_builder_instr_insert(b, &phi_result->instr);
+
+      /* Loop increment src for phi_iter_ssa. */
+      nir_def *next_iter = nir_iadd(b, phi_iter_ssa, iter_incr);
       nir_phi_instr_add_src(phi_iter,
-                            loop_incr->parent_instr->block,
-                            loop_incr);
+                            nir_cursor_current_block(b->cursor),
+                            next_iter);
    }
+   per_inst_loop->control = nir_loop_control_dont_unroll;
    nir_pop_loop(b, per_inst_loop);
 
-   if (locked)
+   if (locked) {
       nir_mutex_img(b,
                     .mutex_id_img = ROGUE_MUTEX_ID_ATOMIC_EMU,
                     .mutex_op_img = ROGUE_MUTEX_OP_RELEASE);
+   }
 
    b->cursor = nir_before_cf_list(&per_inst_loop->body);
    nir_builder_instr_insert(b, &phi_iter->instr);
+   nir_builder_instr_insert(b, &phi_result_out->instr);
 
-   return op_result;
+   *per_inst_op = dummy_op;
+   *loop_result = phi_result_out_ssa;
+}
+
+static nir_def *emit_atomic_op(nir_builder *b,
+                               nir_atomic_op op,
+                               nir_def *pre_val,
+                               nir_def *value,
+                               nir_def *value_swap)
+{
+   ASSERTED nir_alu_type type = nir_atomic_op_type(op);
+
+   switch (op) {
+   case nir_atomic_op_iadd:
+      assert(type == nir_type_uint);
+      return nir_iadd(b, pre_val, value);
+
+   case nir_atomic_op_imin:
+      assert(type == nir_type_int);
+      return nir_imin(b, pre_val, value);
+
+   case nir_atomic_op_umin:
+      assert(type == nir_type_uint);
+      return nir_umin(b, pre_val, value);
+
+   case nir_atomic_op_imax:
+      assert(type == nir_type_int);
+      return nir_imax(b, pre_val, value);
+
+   case nir_atomic_op_umax:
+      assert(type == nir_type_uint);
+      return nir_umax(b, pre_val, value);
+
+   case nir_atomic_op_iand:
+      assert(type == nir_type_uint);
+      return nir_iand(b, pre_val, value);
+
+   case nir_atomic_op_ior:
+      assert(type == nir_type_uint);
+      return nir_ior(b, pre_val, value);
+
+   case nir_atomic_op_ixor:
+      assert(type == nir_type_uint);
+      return nir_ixor(b, pre_val, value);
+
+   case nir_atomic_op_xchg:
+      assert(type == nir_type_uint);
+      return nir_mov(b, value);
+
+   case nir_atomic_op_fadd:
+      assert(type == nir_type_float);
+      return nir_fadd(b, pre_val, value);
+
+   case nir_atomic_op_fmin:
+      assert(type == nir_type_float);
+      return nir_fmin(b, pre_val, value);
+
+   case nir_atomic_op_fmax:
+      assert(type == nir_type_float);
+      return nir_fmax(b, pre_val, value);
+
+   case nir_atomic_op_cmpxchg:
+      assert(type == nir_type_uint);
+      assert(value_swap);
+      return nir_bcsel(b, nir_ieq(b, pre_val, value), value_swap, pre_val);
+
+   case nir_atomic_op_fcmpxchg:
+      assert(type == nir_type_float);
+      assert(value_swap);
+      return nir_bcsel(b, nir_feq(b, pre_val, value), value_swap, pre_val);
+
+   default:
+      break;
+   }
+
+   unreachable("Unsupported atomic op.");
 }
 
 static nir_def *
@@ -140,11 +272,10 @@ lower_atomic(nir_builder *b, nir_instr *instr, UNUSED void *_data)
    unsigned num_components = intr->def.num_components;
    assert(num_components == 1);
 
-   unsigned bit_size = intr->def.bit_size;
+   ASSERTED unsigned bit_size = intr->def.bit_size;
    assert(bit_size == 32); /* TODO: support other bit sizes. */
 
    nir_atomic_op op = nir_intrinsic_atomic_op(intr);
-   nir_alu_type type = nir_atomic_op_type(op) | bit_size;
 
    bool is_shared = intr->intrinsic == nir_intrinsic_shared_atomic_img ||
                     intr->intrinsic == nir_intrinsic_shared_atomic_swap_img;
@@ -157,95 +288,30 @@ lower_atomic(nir_builder *b, nir_instr *instr, UNUSED void *_data)
    if (is_swap)
       value_swap = intr->src[2].ssa;
 
-   /**/
+   /* Create per-instance locked loop to emulate the atomic op. */
+   nir_def *atom_result;
+   nir_def *per_inst_op;
+   rogue_per_instance_loop(b,
+                           true,
+                           num_components,
+                           bit_size,
+                           &per_inst_op,
+                           &atom_result);
+   b->cursor = nir_after_instr(per_inst_op->parent_instr);
 
-   nir_def *op_result =
-      rogue_per_instance_loop(b, true, num_components, bit_size);
-   b->cursor = nir_after_instr(op_result->parent_instr);
+   /* Get the pre-op value. */
+   nir_def *pre_val;
+   if (is_shared)
+      pre_val = nir_load_shared_img(b, offset);
+   else
+      pre_val =
+         nir_load_global(b, offset, bit_size / 8, num_components, bit_size);
 
-   nir_def *pre_val =
-      is_shared
-         ? pre_val = nir_load_shared_img(b, offset)
-         : nir_load_global(b, offset, bit_size / 8, num_components, bit_size);
+   /* Emit the atomic op. */
+   nir_def *post_val = emit_atomic_op(b, op, pre_val, value, value_swap);
 
-   nir_def *post_val = NULL;
-   switch (op) {
-   case nir_atomic_op_iadd:
-      assert(type == nir_type_uint32);
-      post_val = nir_iadd(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_imin:
-      assert(type == nir_type_int32);
-      post_val = nir_imin(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_umin:
-      assert(type == nir_type_uint32);
-      post_val = nir_umin(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_imax:
-      assert(type == nir_type_int32);
-      post_val = nir_imax(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_umax:
-      assert(type == nir_type_uint32);
-      post_val = nir_umax(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_iand:
-      assert(type == nir_type_uint32);
-      post_val = nir_iand(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_ior:
-      assert(type == nir_type_uint32);
-      post_val = nir_ior(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_ixor:
-      assert(type == nir_type_uint32);
-      post_val = nir_ixor(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_xchg:
-      assert(type == nir_type_uint32);
-      post_val = nir_mov(b, value);
-      break;
-
-   case nir_atomic_op_fadd:
-      assert(type == nir_type_float32);
-      post_val = nir_fadd(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_fmin:
-      assert(type == nir_type_float32);
-      post_val = nir_fmin(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_fmax:
-      assert(type == nir_type_float32);
-      post_val = nir_fmax(b, pre_val, value);
-      break;
-
-   case nir_atomic_op_cmpxchg:
-      assert(type == nir_type_uint32);
-      post_val = nir_bcsel(b, nir_ieq(b, pre_val, value), value_swap, pre_val);
-      break;
-
-   case nir_atomic_op_fcmpxchg:
-      assert(type == nir_type_float32);
-      post_val = nir_bcsel(b, nir_feq(b, pre_val, value), value_swap, pre_val);
-      break;
-
-   default:
-      unreachable("Unsupported atomic op.");
-   }
-
-   nir_def_rewrite_uses(op_result, post_val);
-   assert(nir_def_is_unused(op_result));
+   /* Replace the dummy loop op with the atomic op. */
+   nir_def_rewrite_uses(per_inst_op, pre_val);
 
    if (is_shared) {
       nir_store_shared_img(b, post_val, offset);
@@ -257,7 +323,7 @@ lower_atomic(nir_builder *b, nir_instr *instr, UNUSED void *_data)
                        BITFIELD_MASK(num_components));
    }
 
-   return pre_val;
+   return atom_result;
 }
 
 static bool is_lowerable_atomic(const nir_instr *instr, const void *data)
