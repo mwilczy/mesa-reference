6c85e20b78adf788aa504617760538eca08bd9fd
Matt Coster <matt.coster@imgtec.com>
2023-09-20 16:34:43 +0100

    pvr: Don't rely on GNU void pointer arithmetic
    Besides being not standard C, one instance (in pvr_cmd_buffer.c) was a
    bug caused by adding-then-casting, which would likely have been caught
    if void pointer arithmetic were not allowed.
    
    All instances detected by -Wpointer-arith have been fixed here.
    
    Signed-off-by: Matt Coster <matt.coster@imgtec.com>

--- src/imagination/vulkan/pvr_device.c
+++ src/imagination/vulkan/pvr_device.c
@@ -2299,7 +2299,7 @@ VkResult pvr_MapMemory(VkDevice _device,
 
    /* Check if already mapped */
    if (mem->bo->map) {
-      *ppData = mem->bo->map + offset;
+      *ppData = (uint8_t *)mem->bo->map + offset;
       return VK_SUCCESS;
    }
 
@@ -2308,7 +2308,7 @@ VkResult pvr_MapMemory(VkDevice _device,
    if (result != VK_SUCCESS)
       return result;
 
-   *ppData = mem->bo->map + offset;
+   *ppData = (uint8_t *)mem->bo->map + offset;
 
    return VK_SUCCESS;
 }
