android-pvr/external_mesa3d

pvr: apply in shader blending with nir_lower_blend

src/imagination/rogue/nir/rogue_nir_pfo.c
@@ -25,8 +25,11 @@
 #include "nir/nir_builder.h"
 #include "nir/nir_search_helpers.h"
 #include "nir/nir_format_convert.h"
+#include "nir/nir_lower_blend.h"
 #include "rogue.h"
 #include "util/macros.h"
+#include "vk_blend.h"
+#include "vk_graphics_state.h"
 #include "vulkan/vk_format.h"
 
 /**
@@ -122,7 +125,6 @@ lower_store_output(nir_builder *b, nir_instr *instr, void *cb_data)
    nir_intrinsic_instr *old_store = nir_instr_as_intrinsic(instr);
    nir_src *output_src = &old_store->src[0];
    unsigned components = nir_src_num_components(*output_src);
-   assert(components == 4);
 
    nir_io_semantics sem = nir_intrinsic_io_semantics(old_store);
    nir_variable *output_var =
@@ -139,8 +141,11 @@ lower_store_output(nir_builder *b, nir_instr *instr, void *cb_data)
    assert(mrt_resource->reg.offset == 0);
 
    enum pvr_pbe_accum_format pbe = fs_data->outputs[mrt_idx].accum_format;
+   ASSERTED enum pipe_format format = fs_data->outputs[mrt_idx].format;
+   assert(components == util_format_get_nr_components(format));
 
-   unsigned pbe_dwords = mrt_resource->intermediate_size;
+   unsigned pbe_dwords =
+      DIV_ROUND_UP(mrt_resource->intermediate_size, sizeof(uint32_t));
 
    const struct glsl_type *glsl_type = pbe_accum_to_glsl_type(pbe);
    nir_alu_type nir_type = nir_get_nir_type_for_glsl_type(glsl_type);
@@ -187,15 +192,121 @@ static bool is_store_output(const nir_instr *instr, UNUSED const void *cb_data)
    return (sem.location >= FRAG_RESULT_DATA0);
 }
 
+static bool
+lower_blend_consts(struct nir_builder *b, nir_instr *instr, void *data)
+{
+   if (instr->type != nir_instr_type_intrinsic)
+      return false;
+
+   nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+   if (intr->intrinsic != nir_intrinsic_load_blend_const_color_rgba)
+      return false;
+
+   assert(intr->dest.is_ssa);
+   nir_ssa_def *def = &intr->dest.ssa;
+
+   b->cursor = nir_before_instr(&intr->instr);
+
+   rogue_build_ctx *ctx = data;
+   struct rogue_fs_build_data *fs_data = &ctx->stage_data.fs;
+   const float *v = fs_data->cb_state->blend_constants;
+
+   const struct pvr_pipeline_layout *pipeline_layout = ctx->pipeline_layout;
+   const struct pvr_sh_reg_layout *sh_reg_layout =
+      &pipeline_layout->sh_reg_layout_per_stage[PVR_STAGE_ALLOCATION_FRAGMENT];
+
+   if (!sh_reg_layout->blend_consts.present) {
+      nir_ssa_def_rewrite_uses(def, nir_imm_vec4(b, v[0], v[1], v[2], v[3]));
+      nir_instr_remove(&intr->instr);
+      return true;
+   }
+
+   /* Yes, this is reused between FS/CS. */
+   nir_ssa_def *consts_base = nir_load_num_workgroups_base_addr_img(b);
+
+   nir_ssa_def *vals[4];
+   for (unsigned c = 0; c < 4; ++c) {
+      vals[c] =
+         nir_f2f32(b,
+                   nir_load_global_constant(b,
+                                            nir_iadd_imm(b, consts_base, 4 * c),
+                                            4,
+                                            1,
+                                            32));
+   }
+
+   nir_ssa_def_rewrite_uses(&intr->dest.ssa, nir_vec(b, vals, 4));
+   nir_instr_remove(&intr->instr);
+   return true;
+}
+
+static void rogue_lower_blend(nir_shader *shader, rogue_build_ctx *ctx)
+{
+   struct rogue_fs_build_data *fs_data = &ctx->stage_data.fs;
+   const struct vk_color_blend_state *cb = fs_data->cb_state;
+
+   nir_lower_blend_options opts = {
+      .scalar_blend_const = false,
+      .logicop_enable = cb->logic_op_enable,
+      .logicop_func = vk_logic_op_to_pipe(cb->logic_op),
+   };
+
+   memset(opts.format, 0, sizeof(*opts.format));
+
+   for (int i = 0; i < MIN2(fs_data->num_outputs, cb->attachment_count); i++) {
+      const struct vk_color_blend_attachment_state *rt = &cb->attachments[i];
+      opts.format[i] = fs_data->outputs[i].format;
+
+      if (cb->logic_op_enable) {
+         /* No blending, but we get the colour mask below */
+      } else if (!rt->blend_enable) {
+         static const nir_lower_blend_channel replace = {
+            .func = PIPE_BLEND_ADD,
+            .src_factor = PIPE_BLENDFACTOR_ONE,
+            .dst_factor = PIPE_BLENDFACTOR_ZERO,
+         };
+
+         opts.rt[i].rgb = replace;
+         opts.rt[i].alpha = replace;
+      } else {
+         opts.rt[i].rgb.func = vk_blend_op_to_pipe(rt->color_blend_op);
+         opts.rt[i].rgb.src_factor =
+            vk_blend_factor_to_pipe(rt->src_color_blend_factor);
+         opts.rt[i].rgb.dst_factor =
+            vk_blend_factor_to_pipe(rt->dst_color_blend_factor);
+
+         opts.rt[i].alpha.func = vk_blend_op_to_pipe(rt->alpha_blend_op);
+         opts.rt[i].alpha.src_factor =
+            vk_blend_factor_to_pipe(rt->src_alpha_blend_factor);
+         opts.rt[i].alpha.dst_factor =
+            vk_blend_factor_to_pipe(rt->dst_alpha_blend_factor);
+      }
+
+      opts.rt[i].colormask =
+         (cb->color_write_enables & BITFIELD_BIT(i)) ? rt->write_mask : 0;
+   }
+
+   NIR_PASS_V(shader, nir_lower_blend, &opts);
+
+   nir_shader_instructions_pass(shader,
+                                lower_blend_consts,
+                                nir_metadata_block_index |
+                                   nir_metadata_dominance,
+                                ctx);
+}
+
 PUBLIC
-bool rogue_nir_pfo(nir_shader *shader, struct rogue_fs_build_data *fs_data)
+bool rogue_nir_pfo(nir_shader *shader, rogue_build_ctx *ctx)
 {
    /* Only apply to fragment shaders. */
    if (shader->info.stage != MESA_SHADER_FRAGMENT)
       return false;
 
-   return nir_shader_lower_instructions(shader,
-                                        is_store_output,
-                                        lower_store_output,
-                                        fs_data);
+   rogue_lower_blend(shader, ctx);
+
+   nir_shader_lower_instructions(shader,
+                                 is_store_output,
+                                 lower_store_output,
+                                 &ctx->stage_data.fs);
+   return true;
 }

mwilczy/mesa-reference

src/imagination/rogue/nir/rogue_nir_pfo.c
@@ -25,8 +25,11 @@
 #include "nir/nir_builder.h"
 #include "nir/nir_search_helpers.h"
 #include "nir/nir_format_convert.h"
+#include "nir/nir_lower_blend.h"
 #include "rogue.h"
 #include "util/macros.h"
+#include "vk_blend.h"
+#include "vk_graphics_state.h"
 #include "vulkan/vk_format.h"
 
 /**
@@ -121,7 +124,6 @@ static nir_def *lower_store_output(nir_builder *b, nir_instr *instr, void *cb_da
    nir_intrinsic_instr *old_store = nir_instr_as_intrinsic(instr);
    nir_src *output_src = &old_store->src[0];
    unsigned components = nir_src_num_components(*output_src);
-   assert(components == 4);
 
    nir_io_semantics sem = nir_intrinsic_io_semantics(old_store);
    nir_variable *output_var =
@@ -138,8 +140,11 @@ static nir_def *lower_store_output(nir_builder *b, nir_instr *instr, void *cb_da
    assert(mrt_resource->reg.offset == 0);
 
    enum pvr_pbe_accum_format pbe = fs_data->outputs[mrt_idx].accum_format;
+   ASSERTED enum pipe_format format = fs_data->outputs[mrt_idx].format;
+   assert(components == util_format_get_nr_components(format));
 
-   unsigned pbe_dwords = mrt_resource->intermediate_size;
+   unsigned pbe_dwords =
+      DIV_ROUND_UP(mrt_resource->intermediate_size, sizeof(uint32_t));
 
    const struct glsl_type *glsl_type = pbe_accum_to_glsl_type(pbe);
    nir_alu_type nir_type = nir_get_nir_type_for_glsl_type(glsl_type);
@@ -186,15 +191,120 @@ static bool is_store_output(const nir_instr *instr, UNUSED const void *cb_data)
    return (sem.location >= FRAG_RESULT_DATA0);
 }
 
+static bool
+lower_blend_consts(struct nir_builder *b, nir_instr *instr, void *data)
+{
+   if (instr->type != nir_instr_type_intrinsic)
+      return false;
+
+   nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+   if (intr->intrinsic != nir_intrinsic_load_blend_const_color_rgba)
+      return false;
+
+   nir_def *def = &intr->def;
+
+   b->cursor = nir_before_instr(&intr->instr);
+
+   rogue_build_ctx *ctx = data;
+   struct rogue_fs_build_data *fs_data = &ctx->stage_data.fs;
+   const float *v = fs_data->cb_state->blend_constants;
+
+   const struct pvr_pipeline_layout *pipeline_layout = ctx->pipeline_layout;
+   const struct pvr_sh_reg_layout *sh_reg_layout =
+      &pipeline_layout->sh_reg_layout_per_stage[PVR_STAGE_ALLOCATION_FRAGMENT];
+
+   if (!sh_reg_layout->blend_consts.present) {
+      nir_def_rewrite_uses(def, nir_imm_vec4(b, v[0], v[1], v[2], v[3]));
+      nir_instr_remove(&intr->instr);
+      return true;
+   }
+
+   /* Yes, this is reused between FS/CS. */
+   nir_def *consts_base = nir_load_num_workgroups_base_addr_img(b);
+
+   nir_def *vals[4];
+   for (unsigned c = 0; c < 4; ++c) {
+      vals[c] =
+         nir_f2f32(b,
+                   nir_load_global_constant(b,
+                                            nir_iadd_imm(b, consts_base, 4 * c),
+                                            4,
+                                            1,
+                                            32));
+   }
+
+   nir_def_rewrite_uses(&intr->def, nir_vec(b, vals, 4));
+   nir_instr_remove(&intr->instr);
+   return true;
+}
+
+static void rogue_lower_blend(nir_shader *shader, rogue_build_ctx *ctx)
+{
+   struct rogue_fs_build_data *fs_data = &ctx->stage_data.fs;
+   const struct vk_color_blend_state *cb = fs_data->cb_state;
+
+   nir_lower_blend_options opts = {
+      .scalar_blend_const = false,
+      .logicop_enable = cb->logic_op_enable,
+      .logicop_func = vk_logic_op_to_pipe(cb->logic_op),
+   };
+
+   memset(opts.format, 0, sizeof(*opts.format));
+
+   for (int i = 0; i < MIN2(fs_data->num_outputs, cb->attachment_count); i++) {
+      const struct vk_color_blend_attachment_state *rt = &cb->attachments[i];
+      opts.format[i] = fs_data->outputs[i].format;
+
+      if (cb->logic_op_enable) {
+         /* No blending, but we get the colour mask below */
+      } else if (!rt->blend_enable) {
+         static const nir_lower_blend_channel replace = {
+            .func = PIPE_BLEND_ADD,
+            .src_factor = PIPE_BLENDFACTOR_ONE,
+            .dst_factor = PIPE_BLENDFACTOR_ZERO,
+         };
+
+         opts.rt[i].rgb = replace;
+         opts.rt[i].alpha = replace;
+      } else {
+         opts.rt[i].rgb.func = vk_blend_op_to_pipe(rt->color_blend_op);
+         opts.rt[i].rgb.src_factor =
+            vk_blend_factor_to_pipe(rt->src_color_blend_factor);
+         opts.rt[i].rgb.dst_factor =
+            vk_blend_factor_to_pipe(rt->dst_color_blend_factor);
+
+         opts.rt[i].alpha.func = vk_blend_op_to_pipe(rt->alpha_blend_op);
+         opts.rt[i].alpha.src_factor =
+            vk_blend_factor_to_pipe(rt->src_alpha_blend_factor);
+         opts.rt[i].alpha.dst_factor =
+            vk_blend_factor_to_pipe(rt->dst_alpha_blend_factor);
+      }
+
+      opts.rt[i].colormask =
+         (cb->color_write_enables & BITFIELD_BIT(i)) ? rt->write_mask : 0;
+   }
+
+   NIR_PASS_V(shader, nir_lower_blend, &opts);
+
+   nir_shader_instructions_pass(shader,
+                                lower_blend_consts,
+                                nir_metadata_block_index |
+                                   nir_metadata_dominance,
+                                ctx);
+}
+
 PUBLIC
-bool rogue_nir_pfo(nir_shader *shader, struct rogue_fs_build_data *fs_data)
+bool rogue_nir_pfo(nir_shader *shader, rogue_build_ctx *ctx)
 {
    /* Only apply to fragment shaders. */
    if (shader->info.stage != MESA_SHADER_FRAGMENT)
       return false;
 
-   return nir_shader_lower_instructions(shader,
-                                        is_store_output,
-                                        lower_store_output,
-                                        fs_data);
+   rogue_lower_blend(shader, ctx);
+
+   nir_shader_lower_instructions(shader,
+                                 is_store_output,
+                                 lower_store_output,
+                                 &ctx->stage_data.fs);
+   return true;
 }