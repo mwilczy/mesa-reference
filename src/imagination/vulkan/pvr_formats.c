dca8d62f9c447abd7680910ade6b0d48e9141e1d
Vlad Schiller <vlad-radu.schiller@imgtec.com>
2023-12-04 10:13:20 +0000

    pvr: Enable VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT flag
    This is needed for the VK_EXT_extended_dynamic_state tests to pass.
    
    Signed-off-by: Vlad Schiller <vlad-radu.schiller@imgtec.com>

--- src/imagination/vulkan/pvr_formats.c
+++ src/imagination/vulkan/pvr_formats.c
@@ -599,7 +599,8 @@ pvr_get_image_format_features2(const struct pvr_format *pvr_format,
                                          0);
 
          flags |= VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT |
-                  VK_FORMAT_FEATURE_2_BLIT_SRC_BIT;
+                  VK_FORMAT_FEATURE_2_BLIT_SRC_BIT |
+                  VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT;
 
          if (!vk_format_is_int(vk_format) &&
              !vk_format_is_depth_or_stencil(vk_format) &&
@@ -609,7 +610,8 @@ pvr_get_image_format_features2(const struct pvr_format *pvr_format,
          }
       } else if (!vk_format_is_block_compressed(vk_format)) {
          flags |= VK_FORMAT_FEATURE_2_SAMPLED_IMAGE_BIT |
-                  VK_FORMAT_FEATURE_2_BLIT_SRC_BIT;
+                  VK_FORMAT_FEATURE_2_BLIT_SRC_BIT |
+                  VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT;
       }
 
       if (vk_format_has_stencil(vk_format))
@@ -718,6 +720,8 @@ pvr_get_buffer_format_features2(const struct pvr_format *pvr_format)
       return 0;
 
    desc = vk_format_description(vk_format);
+
+   flags |= VK_FORMAT_FEATURE_2_TRANSFER_SRC_BIT;
 
    if (desc->layout == UTIL_FORMAT_LAYOUT_PLAIN &&
        desc->colorspace == UTIL_FORMAT_COLORSPACE_RGB) {
