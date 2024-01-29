814de16cd4fb6a1e1f6ebd2336ad7ac36d96bc13
Frank Binns <frank.binns@imgtec.com>
2024-01-29 11:19:35 +0000

    pvr: add device info for GX6650 (4.46.6.62)
    Requested by the community [1].
    
    [1] https://gitlab.freedesktop.org/mesa/mesa/-/issues/7032
    
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>

--- src/imagination/common/pvr_device_info.c
+++ src/imagination/common/pvr_device_info.c
@@ -32,6 +32,7 @@
 
 #include "device_info/g6110.h"
 #include "device_info/gx6250.h"
+#include "device_info/gx6650.h"
 #include "device_info/axe-1-16m.h"
 #include "device_info/bxe-4-32.h"
 #include "device_info/bxs-4-64.h"
@@ -62,6 +63,7 @@ int pvr_device_info_init(struct pvr_device_info *info, uint64_t bvnc)
    switch (bvnc) {
       CASE_PACKED_BVNC_DEVICE_INFO(4, 40, 2, 51);
       CASE_PACKED_BVNC_DEVICE_INFO(4, 45, 2, 58);
+      CASE_PACKED_BVNC_DEVICE_INFO(4, 46, 6, 62);
       CASE_PACKED_BVNC_DEVICE_INFO(5, 9, 1, 46);
       CASE_PACKED_BVNC_DEVICE_INFO(33, 15, 11, 3);
       CASE_PACKED_BVNC_DEVICE_INFO(36, 50, 54, 182);
